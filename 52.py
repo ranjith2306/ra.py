2d42343daa2d1d923b15f530d9261e45ccf5f72f
ranjith2306 <52270677+ranjith2306@users.noreply.github.com>
2019-07-31 12:45:00 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,23 @@
+f=int(input())
+if f==1:
+	print('one')
+elif f==2:
+	print('two')
+elif f==3:
+	print('three')
+elif f==4:
+	print('four')
+elif f==5:
+	print('five')
+elif f==6:
+	print('six')
+elif f==7:
+	print('seven')
+elif f==8:
+	print('eight')
+elif f==9:
+	print('nine')
+elif f==10:
+	print('ten')
+else:
+	print('invalid')
